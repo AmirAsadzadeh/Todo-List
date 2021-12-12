6ed496208ec89b1e79b5b003155133e6f53bbe71
Amir Ali Asadolahzadeh <amiraliasd79@gmail.com>
2021-12-12 23:01:45 +0330

    hamburger menu style updated!

--- index.js
+++ index.js
@@ -11,15 +11,13 @@ const container = document.getElementById("container");
 collectionBtn.addEventListener("click", () => {
   if (flag === 1) {
     document.querySelector("aside").style.transform = "translateX(-350px)";
-    // document.querySelector("aside").style.position = "absolute";
     container.style.marginLeft = "-300px";
-
+    collectionBtn.classList.remove("active-background");
     flag = 0;
   } else if (flag === 0) {
     document.querySelector("aside").style.transform = "translateX(0px)";
-    // document.querySelector("aside").style.position = "static";
     container.style.marginLeft = "0";
-
+    collectionBtn.classList.add("active-background");
     flag = 1;
   }
 });
