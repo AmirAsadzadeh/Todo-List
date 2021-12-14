75e0140072dd1c4bf284793c9b8c58698ac24dca
Amir Ali Asadolahzadeh <amiraliasd79@gmail.com>
2021-12-14 12:05:53 +0330

    sidebar updated

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ let flag = 1;
 
 const collectionBtn = document.querySelector(".ham-menu");
 
-const container = document.getElementById("container");
-
 //event listeners
 
 collectionBtn.addEventListener("click", () => {
   if (flag === 1) {
-    document.querySelector("aside").style.transform = "translateX(-350px)";
-    container.style.marginLeft = "-300px";
     collectionBtn.classList.remove("active-background");
+    document.querySelector("aside").style.width = "0";
+
     flag = 0;
   } else if (flag === 0) {
-    document.querySelector("aside").style.transform = "translateX(0px)";
-    container.style.marginLeft = "0";
     collectionBtn.classList.add("active-background");
+    document.querySelector("aside").style.width = "300px";
+
     flag = 1;
   }
 });
